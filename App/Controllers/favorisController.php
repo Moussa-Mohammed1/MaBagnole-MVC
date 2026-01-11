3dc96f3d910deed8d67e34fcee3c829759b0ab72
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-01-11 15:30:45 +0100

    refactor(favoris-controller): replace add/delete actions with toggle action

--- App/Controllers/favorisController.php
+++ App/Controllers/favorisController.php
@@ -11,26 +11,19 @@ session_start();
 $action = $_GET['action'] ?? '';
 
 switch ($action) {
-    case 'delete':
+    case 'toggle':
         if (isset($_GET['id_article']) && isset($_GET['id_client'])) {
             $id_article = (int)$_GET['id_article'];
             $id_client = (int)$_GET['id_client'];
 
-            Favoris::deleteFavoris($id_article, $id_client);
-            header('Location: ../Views/Client/blog/favoris.php');
+            if (Favoris::isFavorite($id_article, $id_client)) {
+                Favoris::deleteFavoris($id_article, $id_client);
+            } else {
+                Favoris::addFavoris($id_article, $id_client);
+            }
+            header('Location: ../Views/Client/blog/ArticlesList.php');
             exit();
-        } 
-        break;
-
-    case 'add':
-        if (isset($_POST['id_article']) && isset($_POST['id_client'])) {
-            $id_article = (int)$_POST['id_article'];
-            $id_client = (int)$_POST['id_client'];
-
-            Favoris::addFavoris($id_article, $id_client);
-            header('Location: ' . $_SERVER['HTTP_REFERER']);
-            exit();
-        } 
+        }
         break;
 
     default:
